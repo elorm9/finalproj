f036d68b5acba78791ea10e6fad143414906831a
elorm9 <elorm9@f54b1630-a70c-6f48-a649-07f75cb78ad8>
2010-06-02 17:14:28 +0000

    Added comments to functions

--- src/src/game/Airplane.java
+++ src/src/game/Airplane.java
@@ -17,8 +17,10 @@ public abstract class Airplane extends Entity{
     private int HP;
     private int maxHP;
     
+    //location of the animated gif containing the explosion image
     private String explosionRef = "Images/Planes/explode.gif";
     
+    //stores the missiles for each airplane
 	private ArrayList <Missile> missiles = new ArrayList<Missile>();
 	
 	private AudioClip laser1;
@@ -26,26 +28,31 @@ public abstract class Airplane extends Entity{
 	private AudioClip laser3;
 	private AudioClip explode;
 	
+	//methods for firing various missiles
     public abstract void fireRedBullet();
     public abstract void fireBlueBullet();
     public abstract void fireMissle();
     
+    //method for drawing HP
     public abstract void drawHP(Graphics hp);
 
-    
+    //default constructor
     public Airplane() {
 
     }
     
+    //sets the location according to the input x and y variables
     public Airplane(int x, int y)
     {
     	setLocation(x,y);
     }
    
+    //returns the arraylist of missiles
     public ArrayList<Missile> getMissiles(){
     	return missiles;
     }
     
+    //draws all the missiles inside the arraylist of each plane
 	public void drawMissiles(Graphics2D g2d, ImageObserver j)
 	{
 		int x = 0;
@@ -60,38 +67,42 @@ public abstract class Airplane extends Entity{
 		}
 	}
 	
+	//reduces the health of the airplane
 	public void takeDamage(int damage)
 	{
 		HP -= damage;
 	}
 	
+	//sets the HP of the airplane
 	public void setHP(int h){
 		HP = h;
 	}
 	
+	//gets the HP of the airplane
 	public int getHP()
 	{
 		return HP;
 	}
 	
+	//sets the maximum possible HP of the airplane
 	public void setMaxHP(int h){
 		maxHP = h;
 	}
 	
+	//increases the HP of the airplane by the input amount
+	//also ensures that the airplane's HP doesn't exceed the maximum HP it can have
 	public void increaseHP(int h){
 		HP += h;
 		
 		if(HP > maxHP)
 			HP = maxHP;
 	}
-
+	
+	//returns the location of the reference to the explosion image
 	public String getExplosionRef(){
 		return explosionRef;
 	}
-	
-	public void explode(){
-		super.explode();
-	}
+
 	
 	public void initSounds(){
 		
@@ -107,6 +118,7 @@ public abstract class Airplane extends Entity{
 		url = this.getClass().getResource("Sounds/Explode.aif");
 		explode = Applet.newAudioClip(url);
 	}
+	
 	public AudioClip getL1(){
 		return laser1;
 	}
